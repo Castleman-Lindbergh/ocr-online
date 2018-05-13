8f62956dbb9c8048d3ff560b583c79a5301d1108
Thomas Castleman <thomas7rex@gmail.com>
2018-05-12 20:18:37 -0400

    Basic drawing 28x28 images

--- views/client.js
+++ views/client.js
@@ -1,6 +1,53 @@
 
-var socket = io();
+// var socket = io();
+// socket.on('net', function(data) {
+// 	net = new NeuralNetwork(data.network);
+// });
 
-socket.on('net', function(data) {
-	console.log(data.network);
-});
+var IMAGE_RES = 28;
+var net;
+var w;
+
+var handwriting = new Array(IMAGE_RES);
+for (var i = 0; i < IMAGE_RES; i++) {
+	handwriting[i] = new Array(IMAGE_RES);
+	for (var j = 0; j < IMAGE_RES; j++) {
+		handwriting[i][j] = 0;
+	}
+}
+
+function setup() {
+	var canvas = createCanvas(windowWidth / 2, windowHeight);
+	w = width / IMAGE_RES;
+	canvas.position(0, 0);
+	canvas.style('z-index', '-1');
+}
+
+function draw() {
+	drawGrid();
+}
+
+function drawGrid() {
+	for (var r = 0; r < handwriting.length; r++) {
+		for (var c = 0; c < handwriting[r].length; c++) {
+			fill(handwriting[r][c]);
+			rect(c * w, r * w, w, w);
+		}
+	}
+}
+
+function mouseDragged() {
+	for (var r = 0; r < handwriting.length; r++) {
+		for (var c = 0; c < handwriting[r].length; c++) {
+			if (mouseX > c * w && mouseX < c * w + w) {
+				if (mouseY > r * w && mouseY < r * w + w) {
+					handwriting[r][c] = 255;
+
+					if (r + 1 < IMAGE_RES) handwriting[r + 1][c] = 255;
+					if (c - 1 > -1) handwriting[r][c - 1] = 255;
+					if (r + 1 < IMAGE_RES && c - 1 > -1) handwriting[r + 1][c - 1] = 255;
+				}
+			}
+		}
+	}
+}
